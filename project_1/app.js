b1f8b09128cde813ec47292d3a7e9e80b84245f2
gapinska <gapinska.mon@gmail.com>
2019-08-30 14:53:28 +0200

    after click adding button with a number

--- project_1/app.js
+++ project_1/app.js
@@ -1,6 +1,11 @@
 const btn = document.querySelector("button")
+let number = 1
 const addElement = function(){
+    
     const div= document.createElement('div')
+    div.textContent = number
     document.body.appendChild(div)
+    
+    number= number+1
     }
 btn.addEventListener("click", addElement)
