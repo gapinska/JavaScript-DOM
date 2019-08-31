98d91fde36dd272948ffb4b7e6906ec0570cf2fb
gapinska <gapinska.mon@gmail.com>
2019-09-01 00:24:13 +0200

    changing square size while scrolling

--- project_3/main.js
+++ project_3/main.js
@@ -1,13 +1,26 @@
 const square = document.createElement('div')
 document.body.appendChild(square)
+let grow=true
 
 let size = 100;
 square.style.width = size +"px"
 square.style.height = size +"px"
 
 window.addEventListener("scroll", function () {
+    if(grow ){
     size +=10
     square.style.width = size + "px"
     square.style.height = size +"px"
+    }else{
+        size-=10
+        square.style.width = size + "px"
+        square.style.height = size +"px"
+    }
+    if (size >= this.window.innerWidth/2){
+        grow=false
+    }
+    else if(size===0){
+        grow=true
+    }
 
 })
