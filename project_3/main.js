a82c1b4d83f8a3016fd14deffd1888d22516f849
gapinska <gapinska.mon@gmail.com>
2019-08-31 20:42:19 +0200

    square modified

---
+++ project_3/main.js
@@ -0,0 +1,8 @@
+const square = document.createElement('div')
+document.body.appendChild(square)
+square.style.width = "100px"
+square.style.height = "100px"
+
+window.addEventListener("scroll", function () {
+    
+})
