251cce6198d60362bfd80c642783a677d26436e1
gapinska <gapinska.mon@gmail.com>
2019-08-31 17:53:47 +0200

    add big class to each item which is dividable by 3

--- project_2/app.js
+++ project_2/app.js
@@ -4,6 +4,11 @@ btn.addEventListener("click", function(){
     const listItem = document.createElement("li")
     listItem.textContent = number
     document.body.appendChild(listItem)
+    if(number%3===0){
+        listItem.classList.add("big")
+    }
+
     number +=2
 
+    
 })
