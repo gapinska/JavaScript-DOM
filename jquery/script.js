014aaf88c47a55de58f6e2667d66f83c850a04c5
gapinska <gapinska.mon@gmail.com>
2019-09-01 14:39:55 +0200

    using jquery

--- jquery/script.js
+++ jquery/script.js
@@ -1,2 +1,5 @@
 $("header nav li:first").css({border: "2px solid red"})
 $("header nav li:last").css({border: "2px solid red"})
+
+$("#contact ul:first").css({border: "2px solid red"})
+$("#contact ul:last").css({border: "2px solid red"})
