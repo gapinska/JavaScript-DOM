72a8b00af80c63406ab393553f61154797f1df8f
gapinska <gapinska.mon@gmail.com>
2019-09-01 15:01:21 +0200

    adding border to li item

--- jquery/script.js
+++ jquery/script.js
@@ -3,3 +3,6 @@ $("header nav li:last").css({border: "2px solid red"})
 
 $("#contact ul:first").css({border: "2px solid red"})
 $("#contact ul:last").css({border: "2px solid red"})
+
+$("header nav li:even").css({border: "2px solid blue"})
+$("header nav li:odd").css({border: "2px solid yellow"})
