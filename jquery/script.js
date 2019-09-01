354c1ca0c2dc5f1769c123156b0f223ade7f54f4
gapinska <gapinska.mon@gmail.com>
2019-09-01 15:52:37 +0200

    jquery filters

--- jquery/script.js
+++ jquery/script.js
@@ -6,3 +6,13 @@ $("#contact ul:last").css({border: "2px solid red"})
 
 $("header nav li:even").css({border: "2px solid blue"})
 $("header nav li:odd").css({border: "2px solid yellow"})
+
+
+$("section:not('#contact')").css({border: "2px solid green"})
+
+
+$("#social-nav li:lt(3)").css({border: "2px solid blue"})
+$("#social-nav li:gt(2)").css({border: "2px solid blue"})
+
+$("div[class]").css({border: "2px solid pink"})
+$("img[alt=quote]").css({border: "2px solid purple"})
